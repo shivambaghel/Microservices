17631ae9988c4ead4c92d95eb1c01936c28927af
shivambaghel <singhsbaghel96@gmail.com>
2025-02-10 16:04:48 +0530

    Input Validation

--- accounts/src/main/java/com/accounts/exception/GlobalExceptionHandler.java
+++ accounts/src/main/java/com/accounts/exception/GlobalExceptionHandler.java
@@ -22,6 +22,22 @@ import java.util.Map;
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
+    // validation exception handler -> its of ResponseEntityExceptionHandler class
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+        Map<String, String> validationErrors = new HashMap<>();
+        List<ObjectError> validationErrorList = ex.getBindingResult().getAllErrors();
+
+        validationErrorList.forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String validationMsg = error.getDefaultMessage();
+            validationErrors.put(fieldName, validationMsg);
+        });
+        return new ResponseEntity<>(validationErrors, HttpStatus.BAD_REQUEST);
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponseDto> handleGlobalException(Exception exception,
                                                                   WebRequest webRequest) {
